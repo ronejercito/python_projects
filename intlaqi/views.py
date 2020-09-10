c5f19608605455993962d4f22585f469bf411568
ron <ronej.dev@gmail.com>
2020-09-10 21:33:01 +0800

    second commit

--- intlaqi/views.py
+++ intlaqi/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+# pylint: disable=unused-variable
+# pylint: enable=too-many-lines
+
 def projects(request):
     return render(request, 'projects.html', {})
 
 def intl_aqi(request):
-    return render(request, 'intl_aqi.html', {})
+    import json
+    import requests
+
+    api_req = requests.get("https://api.waqi.info/feed/makati/?token=7b8cc61eb74aee354c5818dfce380d7c9a3ca765")
+
+    try:
+        api = json.loads(api_req.content)
+    except Exception as e:
+        api = "Error: No data"
+
+    return render(request, 'intl_aqi.html', {'api' : api })
